24effcadea912d05fcab757475af2c5909e30d97
Mark Cruz <markchriscruz@yahoo.com>
2018-03-12 07:12:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,7 @@
 class CashRegister
   
   attr_accessor :discount,:total
+  @items = []
   
   def initialize(discount = 0)
     @total = 0
@@ -9,6 +10,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
+    @items << title
   end
   
   def apply_discount
@@ -18,6 +20,10 @@ class CashRegister
       @total -= @total * @discount / 100
       return "After the discount, the total comes to $#{@total}."
     end
+  end
+  
+  def items
+    return @items
   end
   
 end
